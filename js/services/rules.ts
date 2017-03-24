3dfe63070a9dd3ec7d360773ea8bcf70fcbd7af2
Matt Bates <matt.bates@manywho.com>
2017-03-24 15:05:28 +0000

    Refactored rule evaluation

--- js/services/rules.ts
+++ js/services/rules.ts
@@ -29,12 +29,12 @@ manywho.offline.rules = class Rules {
 
         for (let comparison of comparisons) {
             if (comparison.rules)
-                result = manywho.offline.rules.evaluatRules(comparison.rules, comparison.comparisonType, state, snapshot);
+                result = manywho.offline.rules.evaluateRules(comparison.rules, comparison.comparisonType, state, snapshot);
 
             if (comparison.comparisons)
                 result = manywho.offline.rules.evaluateComparisons(comparison.comparisons, state, snapshot);
 
-            if (result && comparison.comparisonType === 'OR')
+            if (result && comparison.comparisonType === 'ANY')
                 return true;
         }
 
@@ -45,88 +45,111 @@ manywho.offline.rules = class Rules {
         let result = false;
 
         for (let rule of rules) {
+            let contentType = manywho.component.contentTypes.string;
+
             let left = snapshot.getValue(rule.leftValueElementToReferenceId);
             left = state.getValue(rule.leftValueElementToReferenceId, left.typeElementId, left.contentType) || left;
 
             let right = snapshot.getValue(rule.rightValueElementToReferenceId);
             right = state.getValue(rule.rightValueElementToReferenceId, right.typeElementId, right.contentType) || right;
 
-            result = manywho.offline.rules.comapreValues(left, right, rule.criteriaType);
+            result = manywho.offline.rules.compareValues(left, right, contentType, rule.criteriaType);
 
-            if (result && criteriaType === 'OR')
+            if (result && criteriaType === 'ANY')
                 return true;
         }
 
         return result;
     }
 
-    static compareValues(left, right, criteriaType) {
-        const contentType: string = 'CONTENTSTRING';
+    static compareValues(left, right, contentType, criteriaType) {
+        switch (contentType) {
+            case 'CONTENTEOBJECT':
+                return manywho.offline.rules.compareObjects(left, right, criteriaType);
+            case 'CONTENTLIST':
+                return manywho.offline.rules.compareLists(left, right, criteriaType);
+            default:
+                const values = manywho.offline.rules.getContentValues(left, right, contentType);
+                return manywho.offline.rules.compareContentValues(values.left, values.right, criteriaType);
+        }
+    }
+
+    static getContentValues(left, right, contentType) {
+        const leftContentValue = left.defaultContentValue || left.contentValue;
+        const rightContentValue = right.defaultContentValue || right.contentValue;
 
         switch (contentType) {
             case 'CONTENTSTRING':
-                return manywho.offline.rules.compareStrings(left.contentValue, right.contentValue, criteriaType);
+            case 'CONTENTCONTENT':
+            case 'CONTENTPASSWORD':
+            case 'CONTENTENCRYPTED':
+                return {
+                    left: leftContentValue ? leftContentValue.toUpperCase() : leftContentValue,
+                    right: rightContentValue ? rightContentValue.toUpperCase() : rightContentValue
+                };
             case 'CONTENTNUMBER':
-                return manywho.offline.rules.compareNumbers(left.contentValue, right.contentValue, criteriaType);
+                return {
+                    left: leftContentValue ? parseFloat(leftContentValue) : leftContentValue,
+                    right: rightContentValue ? parseFloat(rightContentValue) : rightContentValue
+                };
             case 'CONTENTDATETIME':
-                return manywho.offline.rules.compareDateTimes(left.contentValue, right.contentValue, criteriaType);
+               return {
+                    left: leftContentValue ? moment(leftContentValue) : leftContentValue,
+                    right: rightContentValue ? moment(rightContentValue) : rightContentValue
+                };
             case 'CONTENTBOOLEAN':
-                return manywho.offline.rules.compareBooleans(left.contentValue, right.contentValue, criteriaType);
+                return {
+                    left: leftContentValue ? new Boolean(leftContentValue) : leftContentValue,
+                    right: rightContentValue ? new Boolean(rightContentValue) : rightContentValue
+                };
         }
     }
 
-    static compareStrings(left, right, criteriaType) {
-        left = left ? left.toUpperCase() : left;
-        right = right ? right.toUpperCase() : right;
-
+    static compareContentValues(left, right, criteriaType) {
         switch (criteriaType.toUpperCase()) {
             case 'EQUAL':
                 return left === right;
 
-            case 'NOTE_EQUAL':
+            case 'NOT_EQUAL':
                 return  left !== right;
+
+            case 'GREATER_THAN':
+                return left > right;
+
+            case 'GREATER_THAN_OR_EQUAL':
+                return left >= right;
+
+            case 'LESS_THAN':
+                return left < right;
+
+            case 'LESS_THAN_OR_EQUAL':
+                return left <= right;
+
+            case 'STARTS_WITH':
+                return left.startsWith(right);
+
+            case 'ENDS_WITH':
+                return left.endsWith(right);
+
+            case 'CONTAINS':
+                return left.indexOf(right) !== -1;
+
+            case 'IS_EMPTY':
+                return manywho.utils.isNullOrEmpty(left);
         }
     }
 
-    static compareNumbers(left, right, criteriaType) {
-        left = left ? parseFloat(left) : left;
-        right = right ? parseFloat(right) : right;
-
+    static compareObjects(left, right, criteriaType) {
         switch (criteriaType.toUpperCase()) {
-            case 'EQUAL':
-                return left === right;
-
-            case 'NOTE_EQUAL':
-                return  left !== right;
+            case 'IS_EMPTY':
+                return true;
         }
     }
 
-    static compareDateTimes(left, right, criteriaType) {
-        left = left ? moment(left) : left;
-        right = right ? moment(right) : right;
-
+    static compareLists(left, right, criteriaType) {
         switch (criteriaType.toUpperCase()) {
-            case 'EQUAL':
-                return left === right;
-
-            case 'NOTE_EQUAL':
-                return  left !== right;
+            case 'IS_EMPTY':
+                return true;
         }
     }
-
-    static compareBooleans(left, right, criteriaType) {
-        left = left ? new Boolean(left) : left;
-        right = right ? new Boolean(right) : right;
-
-        switch (criteriaType.toUpperCase()) {
-            case 'EQUAL':
-                return left === right;
-
-            case 'NOTE_EQUAL':
-                return  left !== right;
-        }
-
-        return false;
-    }
-
 };
