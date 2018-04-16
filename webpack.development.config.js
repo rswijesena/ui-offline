4294c2c4a14619a274289b20e9f7d6eafacd55d0
jamesbrattBoomi <james_bratt@dell.com>
2018-04-16 13:41:57 +0100

    CORE-3991 - Separate webpack configs for prod and dev

---
+++ webpack.development.config.js
@@ -0,0 +1,48 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path');
+
+const config = {
+    entry: './js/index.js',
+    devtool: 'inline-source-map',
+    module: {
+        noParse: [new RegExp('node_modules/localforage/dist/localforage.js')],
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {loader: 'style-loader'}, 
+                    {loader: 'css-loader'},
+                    {loader: 'less-loader'}
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'ui-offline.js',
+    },
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ],
+    watch: true,
+    watchOptions: {
+        poll: true
+    }
+};
+
+module.exports = (env) => {
+    var defaultDirectory = 'build';
+
+    if (env && env.build)
+        defaultDirectory = env.build;
+
+    config.output.path = path.resolve(__dirname, defaultDirectory, 'js');
+    return config;
+};
---
+++ webpack.production.config.js
@@ -0,0 +1,52 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const pathsToClean = [
+    'dist'
+];
+
+const config = {
+    entry: './js/index.js',
+    devtool: 'inline-source-map',
+    module: {
+        noParse: [new RegExp('node_modules/localforage/dist/localforage.js')],
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {loader: 'style-loader'}, 
+                    {loader: 'css-loader'},
+                    {loader: 'less-loader'}
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            sourceMap: true
+        }),
+        new CleanWebpackPlugin(pathsToClean),
+    ],
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'ui-offline-[chunkhash].js',
+    }
+};
+
+module.exports = (env) => {
+    var defaultDirectory = 'dist';
+
+    if (env && env.build)
+        defaultDirectory = env.build;
+
+    config.output.path = path.resolve(__dirname, defaultDirectory, 'js');
+    return config;
+};
