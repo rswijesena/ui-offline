692d4c3d9902c2e7d8b9e38dca6a3986614127a2
jamesbrattBoomi <james_bratt@dell.com>
2018-08-01 16:28:04 +0100

    CORE-4550 - Defining macro functions for modifying objects

--- workers/Worker.ts
+++ workers/Worker.ts
@@ -1,12 +1,68 @@
 const ctx: Worker = self as any;
 
 const executeMacro = (macro, metadata) => {
+
     const state = {
         setDateTimeValue: (value, operation) => {
             const valueObject = getValueByName(value.replace(/[^a-zA-Z ]/g, ''), metadata);
             return { valueId: valueObject.id, newContentValue: operation };
         },
+        createObject: (type) => {
+            return;
+        },
+        getArray: (value) => {
+            return;
+        },
+        getBooleanValue: (value) => {
+            return;
+        },
+        getContentValue: (value) => {
+            return;
+        },
+        getDateTimeValue: (value) => {
+            return;
+        },
+        getNumberValue: (value) => {
+            return;
+        },
+        getObject: (value) => {
+            return;
+        },
+        getPasswordValue: (value) => {
+            return;
+        },
+        getStringValue: (value) => {
+            return;
+        },
+        getValue: (value) => {
+            return;
+        },
+        setArray: (value, objectData) => {
+            return;
+        },
+        setBooleanValue: (value, boolean) => {
+            return;
+        },
+        setContentValue: (value, content) => {
+            return;
+        },
+        setNumberValue: (value, number) => {
+            return;
+        },
+        setObject: (value, objectData) => {
+            return;
+        },
+        setPasswordValue: (value, password) => {
+            return;
+        },
+        setStringValue: (value, string) => {
+            return;
+        },
+        setValue: (value, string) => {
+            return;
+        },
     };
+
     return Function('"use strict";return (' + macro + ')')()(
         state,
     );
