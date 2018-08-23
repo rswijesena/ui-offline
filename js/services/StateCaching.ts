e23b8e5f9d9717e6801ad65cb7e94f8588b71d18
jamesbrattBoomi <james_bratt@dell.com>
2018-08-23 13:56:17 +0100

    CORE-4588 - Tidying up page component state module

--- js/services/StateCaching.ts
+++ js/services/StateCaching.ts
@@ -1,39 +1,49 @@
-// Initial state store for page component values that have
-// been set whilst the user has gone through the flow online.
-
-// TODO: think this stuff will need to be cached inside indexdb
-// else it is lost during a complete page reload.
-
-// TODO: might be worth using redux for this stuff as
-// well as the the other stateful parts (offline state)
 declare const manywho: any;
 
 let interval = null;
-const cachedComponents = {};
+let cachedComponents = {};
 
+/**
+ * @param flowKey
+ * @description extracting page component properties from
+ * runtime state and storing in a new state that gets updated
+ * via the set interval
+ */
 export const extractComponentValues = (flowKey) => {
     const components = manywho.state.getComponents(flowKey);
-    console.log('Caching components');
-
-    for (const key of Object.keys(components)) {
-        cachedComponents[key] = components[key];
+    if (components) {
+        const clone = {};
+        for (const key of Object.keys(components)) {
+            clone[key] = components[key];
+        }
+        cachedComponents = clone;
     }
 };
 
+/**
+ * @description Stopping the set interval,
+ * this happens when going offline
+ */
 export const killCachingInterval = () => {
-    console.log('Killing cache interval');
     clearInterval(interval);
 };
 
+/**
+ * @description returns an object containing page components,
+ * whereby the key is the component id and the value describes
+ * the component properties
+ */
 export const getCachedValues = () => {
-    console.log('Getting cached components');
     return cachedComponents;
 };
 
+/**
+ * @param flowKey
+ * @description for updating the page component state
+ * periodically based on a time set by the flow builder
+ */
 export const setCachingInterval = (flowKey) => {
-
-    // TODO: look at possibly calling the caching function
-    // recursively from within a setTimeout instead
-    // in case the caching takes longer than the defined interval.
-    interval = setInterval(() => { extractComponentValues(flowKey); }, 5000); // This needs to be configurable
+    interval = setInterval(
+        () => { extractComponentValues(flowKey); }, 5000,
+    );
 };
