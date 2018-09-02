d85aa2536bd51d4621addce4dcb76f599efa804b
jamesbrattBoomi <james_bratt@dell.com>
2018-09-02 12:18:02 +0100

    CORE-4550 - Testing function for extracting an objectdata properties content value

--- __tests__/services/macros/MacroUtils.test.ts
+++ __tests__/services/macros/MacroUtils.test.ts
@@ -1,12 +1,65 @@
+import { getProperty, setProperty } from '../../../js/services/macros/MacroUtils';
+import { CONTENT_TYPES } from '../../../js/constants';
+
 describe('Macro utilities behaviour', () => {
     // getProperty tests
     test('If property is found then returns the property contentValue', () => {
+        const testTypeId = 'test id';
+        const testContentValue = 'test content value';
+        const value = {
+            properties: [
+                {
+                    contentType: 'ContentString',
+                    contentValue: testContentValue,
+                    developerName: 'test name',
+                    typeElementPropertyId: testTypeId,
+                },
+            ],
+            developerName: 'test',
+        };
+
+        const result = getProperty(testTypeId, CONTENT_TYPES.STRING, value);
+        expect(result).toEqual(testContentValue);
     });
     test('If property is found but is incorrect contentType then error is thrown', () => {
+        const testTypeId = 'test id';
+        const testContentValue = 'test content value';
+        const value = {
+            properties: [
+                {
+                    contentType: 'ContentString',
+                    contentValue: testContentValue,
+                    developerName: 'test name',
+                    typeElementPropertyId: testTypeId,
+                },
+            ],
+            developerName: 'test',
+        };
+
+        expect(() => {
+            getProperty(testTypeId, CONTENT_TYPES.NUMBER, value);
+        }).toThrow(Error);
     });
-    test('If objectdata has no properties key key throw an error', () => {
+    test('If objectdata has no properties key throw an error', () => {
+        const testTypeId = 'test id';
+        const value = {
+            developerName: 'test',
+        };
+
+        expect(() => {
+            getProperty(testTypeId, CONTENT_TYPES.NUMBER, value);
+        }).toThrow(Error);
     });
     test('If objectdata properties key is null then throw an error', () => {
+        const testTypeId = 'test id';
+        const value = {
+            properties: null,
+            developerName: 'test',
+        };
+
+        expect(() => {
+            getProperty(testTypeId, CONTENT_TYPES.NUMBER, value);
+        }).toThrow(Error);
     });
 
     // getValueByName tests
