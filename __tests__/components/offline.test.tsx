e30a9de1015d44a8bd60739f3c3f3ff6d9749953
jamesbrattBoomi <james_bratt@dell.com>
2018-09-23 10:52:57 +0100

    CORE-4587 - Testcases will actually be only temporarily applicable because of issue 4590

--- __tests__/components/offline.test.tsx
+++ __tests__/components/offline.test.tsx
@@ -17,26 +17,4 @@ describe('Offline component behaviour', () => {
     test('Offline component renders without crashing', () => {
         expect(componentWrapper.length).toEqual(1);
     });
-
-    test('When page loads flow is set into offline mode if there is an entry in indexdb for current state', () => {
-    });
-
-    test('Offline initialization occurs only when a state token is present in state and if initialization has not already occured', () => {
-    });
-
-    test('When page loads flow is set into offline mode if there is an entry in indexdb for current state', () => {
-    });
-
-    test('Caching object data only occurs periodically when object data requests are present in the snapshot', () => {
-    });
-
-    test('If caching object data requests that a loader spinner is present', () => {
-    });
-
-    test('Callback triggers caching loader spinner to disappear', () => {
-    });
-
-    test('Callback sets a timeout to cache object data request responses based on defined interval value', () => {
-    });
-
 });
