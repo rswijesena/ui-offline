832d12872bf2dc5b69f31c36fde9f04cc2066d2a
jamesbrattBoomi <james_bratt@dell.com>
2018-10-08 15:01:56 +0100

    CORE-4733 - testing the object data caching behaviour in the base component

--- __tests__/components/AppMethods.test.tsx
+++ __tests__/components/AppMethods.test.tsx
@@ -2,6 +2,21 @@ import * as React from 'react';
 import { mount } from 'enzyme';
 import App from '../../js/components/App';
 import OfflineCore from '../../js/services/OfflineCore';
+import ObjectDataCaching from '../../js/services/cache/ObjectDataCaching';
+
+const windowAny:any = window;
+
+jest.mock('../../js/services/Storage', () => ({
+    getOfflineData: jest.fn(() => {
+        return new Promise((resolve, reject) => {
+            resolve(undefined);
+        });
+    }),
+}));
+
+jest.mock('../../js/services/cache/ObjectDataCaching');
+
+const ObjectDataCachingMock:any = ObjectDataCaching;
 
 describe('Base app component behaviour when no data is cached', () => {
     let wrapper = null;
@@ -10,6 +25,9 @@ describe('Base app component behaviour when no data is cached', () => {
     };
 
     beforeEach(() => {
+        windowAny.manywho.state.getState.mockImplementation(() => {
+            return { token: null };
+        });
         wrapper = mount(<App {...props} />);
     });
 
@@ -22,12 +40,21 @@ describe('Base app component behaviour when no data is cached', () => {
         expect(spyCache).toHaveBeenCalled();
     });
 
-    test('Initialization will set flow data in offline state and automatically cache object data', () => {
+    test('Caching object causes loading spinner to display', () => {
         const wrapperInstance = wrapper.instance();
-        const spyInit = jest.spyOn(OfflineCore, 'initialize');
-        const spyCache = jest.spyOn(wrapperInstance, 'cacheObjectData');
-        wrapperInstance.initialize();
-        expect(spyInit).toHaveBeenCalled();
-        expect(spyCache).toHaveBeenCalled();
+        wrapperInstance.flow = {};
+        ObjectDataCachingMock.mockReturnValue(true);
+        wrapperInstance.cacheObjectData();
+        wrapper.update();
+        expect(wrapper.find('.caching-spinner').exists()).toEqual(true);
+    });
+
+    test('If no data to cache then loading spinner is not displayed', () => {
+        const wrapperInstance = wrapper.instance();
+        wrapperInstance.flow = {};
+        ObjectDataCachingMock.mockReturnValue(false);
+        wrapperInstance.cacheObjectData();
+        wrapper.update();
+        expect(wrapper.find('.caching-spinner').exists()).toEqual(false);
     });
 });
